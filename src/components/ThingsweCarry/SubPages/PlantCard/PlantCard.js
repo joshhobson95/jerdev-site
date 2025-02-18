611e54caf6f80fb11ce21ecf11209ba9aa014b63
joshhobson95 <joshuahobson1995@gmail.com>
2025-02-18 13:37:51 -0700

    card css

--- src/components/ThingsweCarry/SubPages/PlantCard/PlantCard.js
+++ src/components/ThingsweCarry/SubPages/PlantCard/PlantCard.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './PlantCard.css'
+import fullsun from '../../../../Assets/icons/FullSun.png'
+import highwater from '../../../../Assets/icons/HighWater.png'
 
 
 function PlantCard() {
@@ -12,15 +14,30 @@ function PlantCard() {
         <img className='plant_card_image' src='https://jericho-content.nyc3.cdn.digitaloceanspaces.com/VARIOUS/SEO_photos/Trees/conifer/Pinon.jpg' />
         <h2>Spanish Broom</h2>
         <h3>(Spartium junceum)</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum  aliquam fermentum sapien ac semper. Fusce et semper ante.  </p>
+        <div className='plant_card_paragraph'>
+        <p>Spanish Broom is a vigarous decidious shrub that thrives in New Mexico. Recognizable by its bright yellow flowers, and unique branch shape, this plant is a great option for shrubbery in any yard. </p>
+        </div>
         </div>
         <div className='plant_card_bottom'>
-            <p>Full Sun</p>
-            <p>Moderate Water</p>
-            <p>NM Native</p>
-            <p>Mature Size: 10' x 10'</p>
-            <p>Blooms: Jun-Jul</p>
-            <p>Zones: 6, 7, 8</p>
+          <div className='card_icon_sub_section'>
+           <img className='sun_icon' src={fullsun}/> <p>Full Sun</p>
+          </div>
+          <div className='card_icon_sub_section'>
+            <img className='water_icon' src={highwater}/>
+            <p>High Water</p>
+          </div>
+          <div className='card_icon_sub_section'>
+            
+            <p>Mature Size:
+          <br></br>
+              10'x10'
+            </p>
+          </div>
+          <div className='card_icon_sub_section'>
+           
+            <p>Zone Hardiness: 6, 7, 8</p>
+          </div>
+ 
 
         </div>
       
--- src/components/ThingsweCarry/SubPages/Trees/Conifer/Conifer.js
+++ src/components/ThingsweCarry/SubPages/Trees/Conifer/Conifer.js
@@ -1,24 +1,12 @@
-import React, { useState } from 'react';
+import React from "react";
 import './Conifer.css'
 import PlantCard from '../../PlantCard/PlantCard';
+
 
 function Conifer() {
 
 
 
-    const [isPineVisible, setIsPineVisible] = useState(false);
-    const [isJuniperVisible, setIsJuniperVisible] = useState(false);
-    const [isSpruceVisible, setIsSpruceVisible] = useState(false);
-    const [isCypressVisible, setIsCypressVisible] = useState(false);
-    const [isFirVisible, setIsFirVisible] = useState(false);
-    const [isCedarVisible, setIsCedarVisible] = useState(false);
-
-    const togglePineVisibility = () => { setIsPineVisible(!isPineVisible); };
-    const toggleJuniperVisibility = () => { setIsJuniperVisible(!isJuniperVisible); };
-    const toggleSpruceVisibility = () => { setIsSpruceVisible(!isSpruceVisible); };
-    const toggleCypressVisibility = () => { setIsCypressVisible(!isCypressVisible); };
-    const toggleFirVisibility = () => { setIsFirVisible(!isFirVisible); };
-    const toggleCedarVisibility = () => { setIsCedarVisible(!isCedarVisible); };
 
 
   return (
@@ -26,97 +14,42 @@ function Conifer() {
 
     <div className='trees_welcome'>
     <h1>New Mexico Conifers</h1>
-  </div>
+    </div>
 
 
 
     <div className='conifer_tree_container'>
       <div className='conifer_top_text'> 
-              <h2>Know your Conifers</h2>
+        <div className='header_background_image'>
+              <h2>New Mexico Conifers</h2>
+        </div>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ac nunc sem. Ut rhoncus tempor magna a ultricies. Nulla facilisi. Nullam fermentum enim quam, sed porttitor mauris ultrices ut. Sed vulputate arcu id suscipit viverra. Vivamus sagittis ligula vitae volutpat blandit. Praesent sagittis felis eu pulvinar fringilla. Ut hendrerit lobortis sapien eget egestas. Nunc varius bibendum consectetur. In egestas nulla a magna condimentum, ornare bibendum ex finibus. Cras commodo leo egestas nisl molestie, vel congue elit vulputate. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut ut gravida justo. Vestibulum vulputate urna eu tortor ultrices gravida. Praesent vel mi ligula. Sed in nisl viverra, convallis nisi ac, luctus augue.</p>
           </div>
-      <div className='tree_examples'>          
-            <div className='conifer_tree_example_each' onClick={togglePineVisibility} style={{ cursor: 'pointer' }}>
-                <h2>Pine</h2>
-                <img className='tree_example_image' src='https://jericho-content.nyc3.cdn.digitaloceanspaces.com/VARIOUS/SEO_photos/Trees/conifer/Pinon.jpg'alt='Pinion at Jericho Nursery in Albuquerque, New Mexico'/>
-                <div className={`tree_exerb ${isPineVisible ? 'visible2' : 'hidden2'}`}>
-                <PlantCard />
+
+
+
+          <div className='tree_catalog_body'>
+            <div className='tree_catalog_sub_section'>
+            <h2 className='tree_catalog_title'>Pine</h2>
+            <div className='catalog_cards'>
+
+
+                     <PlantCard />
+                     <PlantCard />
+
+  
+            </div>
+            </div>
+
+            <div className='tree_catalog_sub_section'>
+            <h2 className='tree_catalog_title'>Juniper</h2>
+            <div className='catalog_cards'>
                     
-                 </div>
-              </div>
-
-              <div className='conifer_tree_example_each' onClick={toggleJuniperVisibility} style={{ cursor: 'pointer' }}>
-                <h2>Juniper</h2>
-                <img className='tree_example_image' src='https://jericho-content.nyc3.cdn.digitaloceanspaces.com/VARIOUS/SEO_photos/Trees/conifer/Juniper.jpg' alt='Juniper at Jericho Nursery in Albuquerque, New Mexico' />
-                <div className={`tree_exerb ${isJuniperVisible ? 'visible2' : 'hidden2'}`}>
-                    <h3>Alligator Juniper</h3>
-                    <p>Quisque vulputate, eros a finibus commodo, felis libero interdum risus, vel efficitur lacus ex id elit. Praesent vel tincidunt elit, in sagittis eros. Curabitur vel dapibus magna, ut elementum ex. Aenean nec augue sit amet arcu bibendum accumsan. Nullam commodo tortor nec tortor rutrum, nec placerat enim tristique. Morbi vitae leo ac arcu tempus volutpat non vel ipsum. Ut vehicula velit quis velit congue, id elementum massa viverra.</p>
-                    <h3>Rocky Mountain Juniper</h3>
-                    <p>Quisque vulputate, eros a finibus commodo, felis libero interdum risus, vel efficitur lacus ex id elit. Praesent vel tincidunt elit, in sagittis eros. Curabitur vel dapibus magna, ut elementum ex. Aenean nec augue sit amet arcu bibendum accumsan. Nullam commodo tortor nec tortor rutrum, nec placerat enim tristique. Morbi vitae leo ac arcu tempus volutpat non vel ipsum. Ut vehicula velit quis velit congue, id elementum massa viverra.</p>
-                    <h3>One-seed Juniper</h3>
-                    <p>Quisque vulputate, eros a finibus commodo, felis libero interdum risus, vel efficitur lacus ex id elit. Praesent vel tincidunt elit, in sagittis eros. Curabitur vel dapibus magna, ut elementum ex. Aenean nec augue sit amet arcu bibendum accumsan. Nullam commodo tortor nec tortor rutrum, nec placerat enim tristique. Morbi vitae leo ac arcu tempus volutpat non vel ipsum. Ut vehicula velit quis velit congue, id elementum massa viverra.</p>
-                </div>
+            </div>
             </div>
 
-            <div className='conifer_tree_example_each' onClick={toggleSpruceVisibility} style={{ cursor: 'pointer' }}>
-                <h2>Spruce</h2>
-                <img className='tree_example_image' src='https://jericho-content.nyc3.cdn.digitaloceanspaces.com/VARIOUS/SEO_photos/Trees/conifer/blueSpruce.jpg' alt='Spruce at Jericho Nursery in Albuquerque, New Mexico' />
-                <div className={`tree_exerb ${isSpruceVisible ? 'visible2' : 'hidden2'}`}>
-                    <h3>Blue Spruce</h3>
-                    <p>Quisque vulputate, eros a finibus commodo, felis libero interdum risus, vel efficitur lacus ex id elit. Praesent vel tincidunt elit, in sagittis eros. Curabitur vel dapibus magna, ut elementum ex. Aenean nec augue sit amet arcu bibendum accumsan. Nullam commodo tortor nec tortor rutrum, nec placerat enim tristique. Morbi vitae leo ac arcu tempus volutpat non vel ipsum. Ut vehicula velit quis velit congue, id elementum massa viverra.</p>
-                    <h3>Engelmann Spruce</h3>
-                    <p>Quisque vulputate, eros a finibus commodo, felis libero interdum risus, vel efficitur lacus ex id elit. Praesent vel tincidunt elit, in sagittis eros. Curabitur vel dapibus magna, ut elementum ex. Aenean nec augue sit amet arcu bibendum accumsan. Nullam commodo tortor nec tortor rutrum, nec placerat enim tristique. Morbi vitae leo ac arcu tempus volutpat non vel ipsum. Ut vehicula velit quis velit congue, id elementum massa viverra.</p>
-                </div>
-            </div>
+          </div>
 
-            <div className='conifer_tree_example_each' onClick={toggleCypressVisibility} style={{ cursor: 'pointer' }}>
-                <h2>Cypress</h2>
-                <img className='tree_example_image' src='https://jericho-content.nyc3.cdn.digitaloceanspaces.com/VARIOUS/SEO_photos/Trees/conifer/Cypress.jpg' alt='Cypress at Jericho Nursery in Albuquerque, New Mexico' />
-                <div className={`tree_exerb ${isCypressVisible ? 'visible2' : 'hidden2'}`}>
-                    <h3>Italian Cypress</h3>
-                    <p>Quisque vulputate, eros a finibus commodo, felis libero interdum risus, vel efficitur lacus ex id elit. Praesent vel tincidunt elit, in sagittis eros. Curabitur vel dapibus magna, ut elementum ex. Aenean nec augue sit amet arcu bibendum accumsan. Nullam commodo tortor nec tortor rutrum, nec placerat enim tristique. Morbi vitae leo ac arcu tempus volutpat non vel ipsum. Ut vehicula velit quis velit congue, id elementum massa viverra.</p>
-                    <h3>Leyland Cypress</h3>
-                    <p>Quisque vulputate, eros a finibus commodo, felis libero interdum risus, vel efficitur lacus ex id elit. Praesent vel tincidunt elit, in sagittis eros. Curabitur vel dapibus magna, ut elementum ex. Aenean nec augue sit amet arcu bibendum accumsan. Nullam commodo tortor nec tortor rutrum, nec placerat enim tristique. Morbi vitae leo ac arcu tempus volutpat non vel ipsum. Ut vehicula velit quis velit congue, id elementum massa viverra.</p>
-                    <h3>Arizona Cypress</h3>
-                    <p>Quisque vulputate, eros a finibus commodo, felis libero interdum risus, vel efficitur lacus ex id elit. Praesent vel tincidunt elit, in sagittis eros. Curabitur vel dapibus magna, ut elementum ex. Aenean nec augue sit amet arcu bibendum accumsan. Nullam commodo tortor nec tortor rutrum, nec placerat enim tristique. Morbi vitae leo ac arcu tempus volutpat non vel ipsum. Ut vehicula velit quis velit congue, id elementum massa viverra.</p>
-                </div>
-            </div>
-
-            <div className='conifer_tree_example_each' onClick={toggleFirVisibility} style={{ cursor: 'pointer' }}>
-                <h2>Fir</h2>
-                <img className='tree_example_image' src='https://jericho-content.nyc3.cdn.digitaloceanspaces.com/VARIOUS/SEO_photos/Trees/conifer/Fir.jpg' alt='Fir at Jericho Nursery in Albuquerque, New Mexico' />
-                <div className={`tree_exerb ${isFirVisible ? 'visible2' : 'hidden2'}`}>
-                    <h3>Douglas Fir</h3>
-                    <p>Quisque vulputate, eros a finibus commodo, felis libero interdum risus, vel efficitur lacus ex id elit. Praesent vel tincidunt elit, in sagittis eros. Curabitur vel dapibus magna, ut elementum ex. Aenean nec augue sit amet arcu bibendum accumsan. Nullam commodo tortor nec tortor rutrum, nec placerat enim tristique. Morbi vitae leo ac arcu tempus volutpat non vel ipsum. Ut vehicula velit quis velit congue, id elementum massa viverra.</p>
-                    <h3>White Fir</h3>
-                    <p>Quisque vulputate, eros a finibus commodo, felis libero interdum risus, vel efficitur lacus ex id elit. Praesent vel tincidunt elit, in sagittis eros. Curabitur vel dapibus magna, ut elementum ex. Aenean nec augue sit amet arcu bibendum accumsan. Nullam commodo tortor nec tortor rutrum, nec placerat enim tristique. Morbi vitae leo ac arcu tempus volutpat non vel ipsum. Ut vehicula velit quis velit congue, id elementum massa viverra.</p>
-                </div>
-            </div>
-
-            <div className='conifer_tree_example_each' onClick={toggleCedarVisibility} style={{ cursor: 'pointer' }}>
-                <h2>Cedar</h2>
-                <img className='tree_example_image' src='https://jericho-content.nyc3.cdn.digitaloceanspaces.com/VARIOUS/SEO_photos/Trees/cedar.jpg' alt='Cedar at Jericho Nursery in Albuquerque, New Mexico' />
-                <div className={`tree_exerb ${isCedarVisible ? 'visible2' : 'hidden2'}`}>
-                    <h3>Atlas Cedar</h3>
-                    <p>Quisque vulputate, eros a finibus commodo, felis libero interdum risus, vel efficitur lacus ex id elit. Praesent vel tincidunt elit, in sagittis eros. Curabitur vel dapibus magna, ut elementum ex. Aenean nec augue sit amet arcu bibendum accumsan. Nullam commodo tortor nec tortor rutrum, nec placerat enim tristique. Morbi vitae leo ac arcu tempus volutpat non vel ipsum. Ut vehicula velit quis velit congue, id elementum massa viverra.</p>
-                    <h3>Deodar Cedar</h3>
-                    <p>Quisque vulputate, eros a finibus commodo, felis libero interdum risus, vel efficitur lacus ex id elit. Praesent vel tincidunt elit, in sagittis eros. Curabitur vel dapibus magna, ut elementum ex. Aenean nec augue sit amet arcu bibendum accumsan. Nullam commodo tortor nec tortor rutrum, nec placerat enim tristique. Morbi vitae leo ac arcu tempus volutpat non vel ipsum. Ut vehicula velit quis velit congue, id elementum massa viverra.</p>
-                </div>
-            </div>
-
-            <div className='conifer_tree_example_each' onClick={toggleCedarVisibility} style={{ cursor: 'pointer' }}>
-                <h2>Arborivitae</h2>
-                <img className='tree_example_image' src='https://jericho-content.nyc3.cdn.digitaloceanspaces.com/VARIOUS/SEO_photos/Trees/cedar.jpg' alt='Cedar at Jericho Nursery in Albuquerque, New Mexico' />
-                <div className={`tree_exerb ${isCedarVisible ? 'visible2' : 'hidden2'}`}>
-                    <h3>Atlas Cedar</h3>
-                    <p>Quisque vulputate, eros a finibus commodo, felis libero interdum risus, vel efficitur lacus ex id elit. Praesent vel tincidunt elit, in sagittis eros. Curabitur vel dapibus magna, ut elementum ex. Aenean nec augue sit amet arcu bibendum accumsan. Nullam commodo tortor nec tortor rutrum, nec placerat enim tristique. Morbi vitae leo ac arcu tempus volutpat non vel ipsum. Ut vehicula velit quis velit congue, id elementum massa viverra.</p>
-                    <h3>Deodar Cedar</h3>
-                    <p>Quisque vulputate, eros a finibus commodo, felis libero interdum risus, vel efficitur lacus ex id elit. Praesent vel tincidunt elit, in sagittis eros. Curabitur vel dapibus magna, ut elementum ex. Aenean nec augue sit amet arcu bibendum accumsan. Nullam commodo tortor nec tortor rutrum, nec placerat enim tristique. Morbi vitae leo ac arcu tempus volutpat non vel ipsum. Ut vehicula velit quis velit congue, id elementum massa viverra.</p>
-                </div>
-            </div>
-
-
-    </div>
   </div>      
 </div>
   )
